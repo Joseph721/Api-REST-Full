62f5f5b9d0867d858c9908bedf4d1262308197e3
Joseph <joseph.chavez2015@gmail.com>
2022-03-01 21:53:38 -0800

    Se creó el usuario para guardar su información en Mongodb

---
+++ models/user_model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+/* Se crea una instancia de mongoose para crear un esquema. */
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        require: true
+    },
+    nombre: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    /* Para cambiar el estado de los datos sin eliminarlos.  */
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    imagen: {
+        type: String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model('Usuario', userSchema);
+
+/* Se crea un modelo de datos.*/
+
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const Usuario = require('../models/user_model');
 const ruta = express.Router();
 
 ruta.get('/', (req, res) => {
     res.json('Listo el GET de usuarios.');
 });
+
+ruta.post('/', (req,res)=>{
+    let body = req.body;
+    let resultado = crearUser(body);
+
+    resultado.then(user=>{
+        res.json({
+            valor: user
+        })
+    }).catch(err => {
+        res.status(400).json({
+            error: err
+        })
+    });
+});
+
+/* Función Asíncrona que permite guardar información de un usuario  */
+async function crearUser(body) {
+    let usuario = new Usuario({
+        email: body.email,
+        nombre: body.nombre,
+        password: body.password
+    });
+    return await usuario.save();
+}
 
 module.exports = ruta;
