429b4346f73c83dce1e47a59233d68dd3cf4aa63
Joseph <joseph.chavez2015@gmail.com>
2022-03-03 19:50:02 -0800

    Se añadió get, put, delete y post a usuarios

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,26 @@ const express = require('express');
 const Usuario = require('../models/user_model');
 const ruta = express.Router();
 
+// ruta.get('/', (req, res) => {
+//     res.json('Listo el GET de usuarios.');
+// });
+
 ruta.get('/', (req, res) => {
-    res.json('Listo el GET de usuarios.');
+    let resultado = listarUsuariosActivos();
+    resultado.then(usuarios =>{
+        res.json(usuarios)
+    }).catch(err =>{
+        res.status(400).json({
+            error: err
+        })
+    });
 });
 
-ruta.post('/', (req,res)=>{
+ruta.post('/', (req, res) => {
     let body = req.body;
     let resultado = crearUser(body);
 
-    resultado.then(user=>{
+    resultado.then(user => {
         res.json({
             valor: user
         })
@@ -21,6 +32,34 @@ ruta.post('/', (req,res)=>{
     });
 });
 
+ruta.put('/:email', (req, res) => {
+    /* Como let "resultado" viene de una función asíncrona, entonces, nos va a devolver una promesa. */
+    let resultado = actualizarUser(req.params.email, req.body);
+    resultado.then(valor => {
+        res.json({
+            valor: valor
+        })
+    }).catch(err => {
+        res.status(400).json({
+            error: err
+        })
+    });
+});
+
+ruta.delete('/:email', (req, res) => {
+    let resultado = desactivarUser(req.params.email);
+    resultado.then(valor => {
+        res.json({
+            usuario: valor
+        })
+    }).catch(err => {
+        res.status(400).json({
+            error: err
+        })
+    });
+});
+
+
 /* Función Asíncrona que permite guardar información de un usuario  */
 async function crearUser(body) {
     let usuario = new Usuario({
@@ -29,6 +68,35 @@ async function crearUser(body) {
         password: body.password
     });
     return await usuario.save();
+}
+
+async function listarUsuariosActivos(){
+    let usuarios = await Usuario.find({"estado":true});
+    return usuarios;
+}
+
+async function actualizarUser(email, body) {
+    /* Se crea un método asíncrono porque vamos a consultar el documento en la base de datos y al mismo tiempo se va a hacer la actualización. */
+
+    /* findOneAndUpdate : Método que selecciona un documento y lo actualiza al mismo tiempo. */
+    let usuario = await Usuario.findOneAndUpdate(email, {
+        $set: {
+            nombre: body.nombre,
+            password: body.password
+        }
+        /* {new: true} Nos retorne el documento que se ha actualizado. */
+    }, { new: true });
+    return usuario;
+}
+
+/* En este caso no se recibe el Body del request ya que solamente se va a hacer una actualización del estado */
+async function desactivarUser(email) {
+    let usuario = await Usuario.findOneAndUpdate(email, {
+        $set: {
+            estado: false
+        }
+    }, { new: true });
+    return usuario;
 }
 
 module.exports = ruta;
